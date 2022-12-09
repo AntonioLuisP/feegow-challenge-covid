202509825ad917a522db6b73ad583277ac219a7c
Antônio Luis <antonioluisp97@gmail.com>
2022-12-08 23:35:08 -0300

    docker ok

--- resources/views/utils/layout/contentHeader.blade.php
+++ resources/views/utils/layout/contentHeader.blade.php
@@ -1,25 +1,23 @@
 @if (isset($no_content))
-    <div class="mt-3"></div>
+    <div class="col-sm-12 mt-3"></div>
 @else
-    <div class="row mb-1">
-        <div class="col-sm-6 col-12">
-            <h3>{{ $title }}</h3>
-        </div>
-        <div class="col-sm-6 col-12">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="{{ route('home') }}">Início</a></li>
-                @foreach ($items as $key => $value)
-                    <li class="breadcrumb-item @if ($loop->last) active @endif ">
-                        @if ($value)
-                            <a href="{{ $value }}">
-                                {{ $key }}
-                            </a>
-                        @else
+    <div class="col-sm-6 col-12">
+        <h3>{{ $title }}</h3>
+    </div>
+    <div class="col-sm-6 col-12">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{ route('home') }}">Início</a></li>
+            @foreach ($items as $key => $value)
+                <li class="breadcrumb-item @if ($loop->last) active @endif ">
+                    @if ($value)
+                        <a href="{{ $value }}">
                             {{ $key }}
-                        @endif
-                    </li>
-                @endforeach
-            </ol>
-        </div>
+                        </a>
+                    @else
+                        {{ $key }}
+                    @endif
+                </li>
+            @endforeach
+        </ol>
     </div>
 @endif
